d426eccda9a5df0a330e3562a0bdddd27af3cd8a
HIMMY20 <chauhanmhimanshu2005@gmail.com>
2025-11-16 16:49:56 +0530

    index file update

--- api/index.js
+++ api/index.js
@@ -1,12 +1,29 @@
-let ex = require("express")
-let app = ex()
-let cors = require("cors")
-let connection = require("../Connection/connection")
-let router = require("../Router/router")
+// let ex = require("express")
+// let app = ex()
+// let cors = require("cors")
+// let connection = require("../Connection/connection")
+// let router = require("../Router/router")
 
-connection()
-app.use(cors())
-app.use(ex.json())
-app.use(router)
+// connection()
+// app.use(cors())
+// app.use(ex.json())
+// app.use(router)
 
-module.exports = app;
+// module.exports = app;
+
+const express = require("express");
+const cors = require("cors");
+const connection = require("../Connection/connection");
+const router = require("../Router/router");
+const serverless = require("serverless-http");
+
+connection();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+// Export as serverless function for Vercel
+module.exports = serverless(app);
